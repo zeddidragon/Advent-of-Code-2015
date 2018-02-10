9eaf94a67378b333e5f00c7aa74dac03e29901bf
Tony Sæle <tas@scoutgg.com>
2018-02-10 16:40:23 +0100

    rename tool to zcrypt

--- day04.sh
+++ day04.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 seed=$(cat input04.txt)
-yes | awk -v seed=$seed '{}{print seed NR}' | ./md5.js | awk -f day04.awk
+yes | awk -v seed=$seed '{}{print seed NR}' | ./zcrypt -a md5 | awk -f day04.awk
