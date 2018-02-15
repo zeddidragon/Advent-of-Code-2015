14b341ab6c009cf9e07154a936f0170c0244e1ef
Tony Sæle <zeddy@Tonys-MacBook-Pro.local>
2018-02-15 02:32:46 +0100

    made replacement osx compatible

--- day07.sh
+++ day07.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 assemble() {
   tr A-Z a-z \
-    | sed -e 's/\bif\b/biff/' -e ''\
+    | sed -e 's/[[:<:]]if[[:>:]]/biff/' -e ''\
     | ./day07.awk \
     | ./bitwise.bc
 }
 
 a=$(cat input07.txt | assemble)
 echo "Part 1: $a"
-b=$(cat input07.txt | sed -e "s/[0-9]\+ -> b$/$a -> b/" | assemble)
+b=$(cat input07.txt | sed -E "s/[0-9]+ -> b\$/$a -> b/" | tee day07.bc | assemble)
 echo "Part 2: $b"
