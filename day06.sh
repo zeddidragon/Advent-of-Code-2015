0c6a61d9ede0740c523864555a7365b0e88dcf99
Tony Sæle <tony@globalit.no>
2018-02-16 16:00:22 +0100

    day 13

--- day06.sh
+++ day06.sh
@@ -7,13 +7,13 @@ cat input06.txt \
   | (
     read answer
     echo "Part 1: $answer"
-  ) \
-& \
+  ) & \
 cat input06.txt \
   | sed -e 's/turn //' -e 's/,/ /g' -e 's/through //' \
   | ./day06.awk -v part=2 \
   | ./grid.bc \
   | (
     read answer
+    wait
     echo "Part 2: $answer"
   )
---
+++ day13.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+cat input13.txt \
+  | ./day13.awk \
+  | sort -nr \
+  | (
+    echo "Part 1: $(head -n 1)"
+  ) & \
+cat input13.txt \
+  | ./day13.awk -v skip=1 insert=Zeddy \
+  | sort -nr \
+  | (
+    echo "Part 2: $(head -n 1)"
+  )
