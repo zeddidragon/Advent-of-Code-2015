c5e78b233d3de21e4c7e6d96cb7d6da15ed2b171
Tony Sæle <tony@globalit.no>
2018-02-16 16:48:57 +0100

    Async more better

--- day06.sh
+++ day06.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
-cat input06.txt \
-  | sed -e 's/turn //' -e 's/,/ /g' -e 's/through //' \
-  | ./day06.awk -v part=1 \
-  | ./grid.bc \
-  | (
-    read answer
-    echo "Part 1: $answer"
-  ) & \
-cat input06.txt \
-  | sed -e 's/turn //' -e 's/,/ /g' -e 's/through //' \
-  | ./day06.awk -v part=2 \
-  | ./grid.bc \
-  | (
-    read answer
-    wait
-    echo "Part 2: $answer"
-  )
+cat <(
+  cat input06.txt \
+    | sed -e 's/turn //' -e 's/,/ /g' -e 's/through //' \
+    | ./day06.awk -v part=1 \
+    | ./grid.bc \
+    | (
+      read answer
+      echo "Part 1: $answer"
+    )
+) <(
+  cat input06.txt \
+    | sed -e 's/turn //' -e 's/,/ /g' -e 's/through //' \
+    | ./day06.awk -v part=2 \
+    | ./grid.bc \
+    | (
+      read answer
+      echo "Part 2: $answer"
+    )
+)
