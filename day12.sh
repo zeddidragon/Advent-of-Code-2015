e934d8bb6c394cd4af6605208b4d72a8ce05cc71
Tony Sæle <tas@scoutgg.com>
2018-02-16 08:48:20 +0100

    part 2

--- day12.sh
+++ day12.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/env bash
-# echo -n "Part 1: "
-# cat input12.json | grep -oE "\-?[0-9]+" | awk '{sum += $1} END{print sum}'
-cat input12.json | ./json.awk
+sum() {
+ grep -oE "\-?[0-9]+$" | awk '{sum += $1} END{print sum}'
+}
+
+echo -n "Part 1: "
+cat input12.json | ./json.awk | sum
+blacklist=$(
+  cat input12.json \
+    | ./json.awk \
+    | grep -E "\"red\"$" \
+    | grep -o "\[.*\"\]" \
+    | grep -oE "(\"?([a-z0-9])+\"?,)+" \
+    | sort \
+    | uniq \
+    | sed -e "s/^/-e '\\\[/" -e "s/$/'/" \
+    | paste -sd " " -
+)
+
+echo -n "Part 2: "
+cmd="grep -v $blacklist"
+cat input12.json \
+  | ./json.awk \
+  | eval $cmd \
+  | sum
