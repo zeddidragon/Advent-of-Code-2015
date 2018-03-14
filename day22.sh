289020f75a62e810d7f332f28c40a0d17164d045
Tony Sæle <tas@scoutgg.com>
2018-03-14 15:37:33 +0100

    paralell more better

--- day22.sh
+++ day22.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-echo -n "Part 1: "
-cat input22.txt | ./day22.awk
-echo -n "Part 2: "
-cat input22.txt | ./day22.awk -v mode=hard
+cat <(
+  echo -n "Part 1: "
+  cat input22.txt | ./day22.awk
+) <(
+  echo -n "Part 2: "
+  cat input22.txt | ./day22.awk -v mode=hard
+)
